94b3a57a2e2f69983f751164621294aeba093fb9
Makurisan <ManfredKubica@web.de>
2022-09-26 11:27:56 +0200

    Delete

--- XPubCnvTRtf/SAddOn/CnvTRtfExpImp.h
+++
@@ -1,8 +0,0 @@
-
-
-#ifdef EXPORT_XPUBCNVTRTF
-#define XPUBCNVTRTF_EXPORTIMPORT __declspec(dllexport)
-#else
-#define XPUBCNVTRTF_EXPORTIMPORT __declspec(dllimport)
-#endif
-
--- XPubMagic/XPubMagicInit.h
+++
@@ -1,3 +0,0 @@
-
-extern void XPubMagicInit(HANDLE hModule, char* pModuleName/*=0*/);
-extern void XPubMagicRelease();
