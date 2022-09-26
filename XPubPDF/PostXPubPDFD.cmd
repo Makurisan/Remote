ec95723734bcb8392ac95cab08eff8b37ff93a4b
Makurisan <ManfredKubica@web.de>
2022-09-26 10:15:19 +0200

    Delete

--- XPubPDF/PostXPubPDFD.cmd
+++
@@ -1,7 +0,0 @@
-mkdir ..\XPublisher\Debug
-copy .\Debug\XPubPDF.dll ..\XPublisher\Debug
-copy .\Debug\XPubPDF.pdb ..\XPublisher\Debug
-
-mkdir ..\XPubWEBService\Debug
-copy .\Debug\XPubPDF.dll ..\XPubWEBService\Debug
-copy .\Debug\XPubPDF.pdb ..\XPubWEBService\Debug
--- XPubPDF/PostXPubPDFR.cmd
+++
@@ -1,5 +0,0 @@
-mkdir ..\XPublisher\Release
-copy .\Release\XPubPDF.dll ..\XPublisher\Release
-
-mkdir ..\XPubWEBService\Release
-copy .\Release\XPubPDF.dll ..\XPubWEBService\Release
